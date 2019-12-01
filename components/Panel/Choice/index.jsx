724405e89cd673fa292211e7e609a7333d330ad0
Pankaj Jasoria <pnkj_jsr@yahoo.co.in>
2019-12-01 22:06:05 +0530

    feat(ui): HomePage | left column added all panel design ui.

--- components/Panel/Choice/index.jsx
+++ components/Panel/Choice/index.jsx
@@ -40,6 +40,194 @@ export class PanelChoice extends Component {
             <Button text="Bad" variant="btn-success" />
           </div>
         </div>
+
+        {/* Action Penal */}
+        <div className="panel-choice">
+          <h2 className="title">Circulate, on my page</h2>
+          <div className="action">
+            <Button text="Circulate" variant="btn-primary" size="btn-sm" />
+            <Button text="Cancel" variant="btn-light" size="btn-sm" />
+          </div>
+        </div>
+
+        {/* Result */}
+        <div className="panel-choice">
+          <div className="result">
+            <div className="top">
+              <figure>
+                <i class="material-icons">account_circle</i>
+              </figure>
+
+              <div className="details">
+                Jagdeep Singh
+                <span>
+                  <i class="material-icons">flag</i>
+                  Aam Aadmi Party
+                </span>
+              </div>
+            </div>
+
+            <div className="bottom">
+              <small>Hari Nagar, Voice</small>
+              <br />
+              <b className="success">95%,</b> people found him amazing.
+              <br />
+              <b className="error">5%,</b> people not found him good.
+            </div>
+          </div>
+        </div>
+
+        {/* Opinion panel */}
+        <div className="panel-choice">
+          <h2 className="title">Your opinion?</h2>
+
+          <ul className="minister-list">
+            <li>
+              <div className="minister-item">
+                <a className="hover" href="">
+                  <ul>
+                    <li>
+                      <div className="feature">
+                        <i></i>
+                        <span>
+                          <b>03</b>
+                          <br />
+                          Cases
+                        </span>
+                      </div>
+                    </li>
+                    <li>
+                      <div className="feature">
+                        <i></i>
+                        <span>
+                          <b>1.85 crore</b>
+                          <br />
+                          Assets
+                        </span>
+                      </div>
+                    </li>
+                  </ul>
+                  <div className="link">Click to select</div>
+                </a>
+
+                <figure>
+                  <i class="material-icons">account_circle</i>
+                </figure>
+                <div className="details">
+                  Jagdeep Singh
+                  <span>
+                    <i class="material-icons">flag</i>
+                    Aam Aadmi Party
+                  </span>
+                </div>
+              </div>
+            </li>
+            <li>
+              <div className="minister-item">
+                <a className="hover" href="">
+                  <ul>
+                    <li>
+                      <div className="feature">
+                        <i></i>
+                        <span>
+                          <b>03</b>
+                          <br />
+                          Cases
+                        </span>
+                      </div>
+                    </li>
+                    <li>
+                      <div className="feature">
+                        <i></i>
+                        <span>
+                          <b>1.85 crore</b>
+                          <br />
+                          Assets
+                        </span>
+                      </div>
+                    </li>
+                  </ul>
+                  <div className="link">Click to select</div>
+                </a>
+
+                <figure>
+                  <i class="material-icons">account_circle</i>
+                </figure>
+                <div className="details">
+                  Jagdeep Singh
+                  <span>
+                    <i class="material-icons">flag</i>
+                    Aam Aadmi Party
+                  </span>
+                </div>
+              </div>
+            </li>
+            <li>
+              <div className="minister-item">
+                <a className="hover" href="">
+                  <ul>
+                    <li>
+                      <div className="feature">
+                        <i></i>
+                        <span>
+                          <b>03</b>
+                          <br />
+                          Cases
+                        </span>
+                      </div>
+                    </li>
+                    <li>
+                      <div className="feature">
+                        <i></i>
+                        <span>
+                          <b>1.85 crore</b>
+                          <br />
+                          Assets
+                        </span>
+                      </div>
+                    </li>
+                  </ul>
+                  <div className="link">Click to select</div>
+                </a>
+
+                <figure>
+                  <i class="material-icons">account_circle</i>
+                </figure>
+                <div className="details">
+                  Jagdeep Singh
+                  <span>
+                    <i class="material-icons">flag</i>
+                    Aam Aadmi Party
+                  </span>
+                </div>
+              </div>
+            </li>
+          </ul>
+        </div>
+
+        {/* Delhi Poll */}
+        <div className="panel-choice poll">
+          <h2 className="title">Delhi want change?</h2>
+
+          <p>Pollution issue should be primary Agenda to solve.</p>
+
+          <div className="action">
+            <Button text="Yes" variant="btn-primary" size="btn-sm" />
+            <Button text="No" variant="btn-danger" size="btn-sm" />
+          </div>
+        </div>
+
+        {/* Area Poll */}
+        <div className="panel-choice poll">
+          <h2 className="title">Hari Nagar, has?</h2>
+
+          <p>Road on near my house is in best condition?</p>
+
+          <div className="action">
+            <Button text="Yes" variant="btn-primary" size="btn-sm" />
+            <Button text="No" variant="btn-danger" size="btn-sm" />
+          </div>
+        </div>
       </Fragment>
     );
   }
