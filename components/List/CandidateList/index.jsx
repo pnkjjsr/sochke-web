bce6f78d5d703f9e9c118361762b46760fe7637b
Pankaj Jasoria <pnkj_jsr@yahoo.co.in>
2019-12-06 22:00:32 +0530

    func: Home | left side bar, candidate data done.

--- components/List/CandidateList/index.jsx
+++ components/List/CandidateList/index.jsx
@@ -1,58 +1,73 @@
 import React, { Component, Fragment } from "react";
 import { connect } from "react-redux";
+import { bindActionCreators } from "redux";
+import candidateActions from "./action";
 
 import "./style.scss";
 
-export class CandidateList extends Component {
+class CandidateList extends Component {
   constructor(props) {
     super(props);
-
-    this.state = {};
+    this.state = {
+      type: props.type,
+      ministers: []
+    };
   }
 
+  static getDerivedStateFromProps(props, state) {
+    const { candidates } = props;
+    let data = candidates.ministers;
+
+    if (data.length) {
+      return { ministers: data };
+    }
+    return null;
+  }
+
+  componentDidMount() {
+    const { type } = this.state;
+    const { candidateAction } = this.props;
+    candidateAction.prefetchMinister(type);
+  }
+
+  loopMinister = () => {
+    const { type, ministers } = this.state;
+    let typeUpperCase = type.toUpperCase();
+    return ministers.map(minister => {
+      if (minister.type == typeUpperCase) {
+        return (
+          <li key={minister.uid}>
+            <a>
+              <div className="candidate">
+                <span>{minister.partyShort}</span>
+                <label htmlFor="Jagdeep Singh">{minister.name}</label>
+                {/* <i className="material-icons">arrow_drop_down</i> */}
+              </div>
+            </a>
+          </li>
+        );
+      }
+    });
+  };
+
   render() {
+    const { type } = this.state;
+
     return (
       <Fragment>
         <div className="candidate-list">
-          <h2 className="title">MLA, Candidates</h2>
+          <h2 className="title">
+            <span>{type}</span>
+          </h2>
 
-          <ul>
-            <li>
-              <a>
-                <div className="candidate">
-                  <span>AAP</span>
-                  <label htmlFor="Jagdeep Singh">Jagdeep Singh</label>
-                  {/* <i class="material-icons">arrow_drop_down</i> */}
-                </div>
-              </a>
-            </li>
-
-            <li>
-              <a>
-                <div className="candidate">
-                  <span>BJP</span>
-                  <label htmlFor="Jagdeep Singh">Avtar Singh Hit</label>
-                  {/* <i class="material-icons">arrow_drop_down</i> */}
-                </div>
-              </a>
-            </li>
-
-            <li>
-              <a>
-                <div className="candidate">
-                  <span>INC</span>
-                  <label htmlFor="Jagdeep Singh">Chander Parkash</label>
-                  {/* <i class="material-icons">arrow_drop_down</i> */}
-                </div>
-              </a>
-            </li>
-          </ul>
+          <ul>{this.loopMinister()}</ul>
         </div>
       </Fragment>
     );
   }
 }
-
-const mapDispatchToProps = {};
+const mapDispatchToProps = dispatch => ({
+  candidateAction: bindActionCreators(candidateActions, dispatch)
+});
 
 export default connect(state => state, mapDispatchToProps)(CandidateList);
--- redux/reducer.js
+++ redux/reducer.js
@@ -4,18 +4,20 @@ import layout from "components/Layout/reducer";
 import notification from "components/Notification/reducer";
 import minister from "components/Panel/Minister/reducer";
 import responds from "components/Respond/RespondList/reducer";
+import candidates from "components/List/CandidateList/reducer";
 
 import login from "pages/login/reducer";
 import register from "pages/register/reducer";
 import account from "pages/account/reducer";
 
 const rootReducer = combineReducers({
-  login,
   layout,
   notification,
   minister,
   responds,
+  candidates,
 
+  login,
   register,
   account
 });
