1be9a15ab5ea961acaf125e7f46ea932b37fa06d
Pankaj Jasoria <pankaj@toaster.co>
2019-12-10 17:00:59 +0530

    feat: Currency string modifier added.

--- utils/stringModifier.jsx
+++ utils/stringModifier.jsx
@@ -1,24 +1,38 @@
 class stringModifier {
-    constructor(props) { }
+  constructor(props) {}
 
-    removeWord = (e, word) => {
-        let arr = [];
-        e.map(i => {
-            let nameBefore = i.officename;
-            let index = nameBefore.search(word)
-            let name = nameBefore.slice(0, index)
-            let state = i.taluk
-            let district = i.districtname
-            let division = i.divisionname
-            arr.push({
-                area: name.trim(),
-                division: division,
-                district: district,
-                state: state
-            })
-        });
-        return arr;
-    }
+  removeWord = (e, word) => {
+    let arr = [];
+    e.map(i => {
+      let nameBefore = i.officename;
+      let index = nameBefore.search(word);
+      let name = nameBefore.slice(0, index);
+      let state = i.taluk;
+      let district = i.districtname;
+      let division = i.divisionname;
+      arr.push({
+        area: name.trim(),
+        division: division,
+        district: district,
+        state: state
+      });
+    });
+    return arr;
+  };
+
+  NumberFormat = e => {
+    new Intl.NumberFormat("en-IN", {
+      style: "currency",
+      currency: "INR"
+    }).format(number);
+
+    new Intl.NumberFormat("en-IN", {
+      style: "currency",
+      currency: "INR",
+      notation: "compact",
+      compactDisplay: "short"
+    }).format(number);
+  };
 }
 
-export default stringModifier
+export default stringModifier;
