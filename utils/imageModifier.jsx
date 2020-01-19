58c3298c653130251b6d2619c8450e2dca5eea1c
Pankaj Jasoria <pnkj_jsr@yahoo.co.in>
2020-01-19 15:48:08 +0530

    fix(func): Photo Upload | image resizer added on , user image, respond image and contribution image.

--- utils/imageModifier.jsx
+++ utils/imageModifier.jsx
@@ -1,8 +1,8 @@
 export default class ImageModifier {
-  resize = (f, h) => {
+  resize = (f, p) => {
     return new Promise((resolve, reject) => {
       const file = f;
-      const height = h;
+      const path = p;
 
       const fileName = file.name;
       const reader = new FileReader();
@@ -13,6 +13,22 @@ export default class ImageModifier {
 
         (img.onload = () => {
           const elem = document.createElement("canvas");
+
+          var height = img.height;
+          switch (path) {
+            case "images/users":
+              if (img.height > 200) height = 200;
+              break;
+            case "images/responds":
+              if (img.height > 600) height = 600;
+              break;
+            case "images/contributions":
+              if (img.height > 800) height = 800;
+              break;
+            default:
+              console.log("Error: path not match with any path");
+          }
+
           const scaleFactor = height / img.height;
           elem.height = height;
           elem.width = img.width * scaleFactor;
