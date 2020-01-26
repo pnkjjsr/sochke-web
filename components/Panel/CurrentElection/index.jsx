a22cc227d35d3f0928206da52b26f6e169058f44
Pankaj Jasoria <pnkj_jsr@yahoo.co.in>
2020-01-26 21:13:52 +0530

    fix: commin soon section added. and title change.

---
+++ components/Panel/CurrentElection/index.jsx
@@ -0,0 +1,17 @@
+import React, { Component, Fragment } from "react";
+import "./style.scss";
+
+class CurrentElection extends Component {
+  render() {
+    const mainClass = "current_election";
+    return (
+      <Fragment>
+        <div className={mainClass}>
+          <span>Comming Soon</span>, 2020 Delhi Candidate.
+        </div>
+      </Fragment>
+    );
+  }
+}
+
+export default CurrentElection;
