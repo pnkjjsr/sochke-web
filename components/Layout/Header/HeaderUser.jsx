9138aa0171c06f94bba147db0e5494247c17e542
Pankaj Jasoria <pnkj_jsr@yahoo.co.in>
2020-01-28 23:11:02 +0530

    fix: global search ui and api working.

--- components/Layout/Header/HeaderUser.jsx
+++ components/Layout/Header/HeaderUser.jsx
@@ -2,6 +2,8 @@ import React, { Fragment, Component } from "react";
 import Link from "next/link";
 import { connect } from "react-redux";
 
+import { service } from "apiConnect";
+
 import UserImage from "components/UserImage";
 import Drawer from "components/Drawer";
 import AccountNav from "components/Nav/Account";
@@ -14,7 +16,11 @@ class HeaderUser extends Component {
     super(props);
     this.state = {
       accountDrawer: "",
-      userDrawer: ""
+      userDrawer: "",
+      classSearch: "",
+      classResult: "",
+      usersData: [],
+      ministersData: []
     };
   }
 
@@ -24,6 +30,7 @@ class HeaderUser extends Component {
       [name]: "open"
     });
   };
+
   handleClose = e => {
     let name = `${e}Drawer`;
     this.setState({
@@ -31,8 +38,64 @@ class HeaderUser extends Component {
     });
   };
 
+  handleClick = () => {
+    this.setState({
+      classSearch: "form__top__active"
+    });
+  };
+
+  handleSubmit = e => {
+    e.preventDefault();
+  };
+
+  handleChange = e => {
+    let keyword = e.target.value;
+
+    service
+      .post("/search", { keyword })
+      .then(res => {
+        let usersLen = res.data.users.length;
+        let ministersLen = res.data.ministers.length;
+
+        if (!usersLen && !ministersLen) return;
+
+        this.setState({
+          classResult: "form__result__show",
+          usersData: res.data.users,
+          ministersData: res.data.ministers
+        });
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  };
+
+  renderLoopUsers = () => {
+    const { usersData } = this.state;
+
+    return usersData.map(user => {
+      return (
+        <li>
+          <a href="">{user.displayName}</a>
+        </li>
+      );
+    });
+  };
+  renderLoopMinisters = () => {
+    const { ministersData } = this.state;
+
+    return ministersData.map(minister => {
+      return (
+        <li>
+          <a href="">{minister.name}</a>
+        </li>
+      );
+    });
+  };
+
   render() {
-    const { accountDrawer, userDrawer } = this.state;
+    const mainClass = "form";
+    const { accountDrawer, userDrawer, classSearch, classResult } = this.state;
     return (
       <Fragment>
         <div className="header bg" role="main">
@@ -63,19 +126,41 @@ class HeaderUser extends Component {
                 </div>
               </div>
 
-              <div className="col-lg-7 d-none">
-                <form>
-                  <div className="form">
-                    <input
-                      className="form-control form__control"
-                      type="text"
-                      placeholder="Search"
-                    />
+              <div className="col-lg-7">
+                <form onSubmit={this.handleSubmit}>
+                  <div className={`${mainClass}`}>
+                    <div className={`${mainClass}__top ${classSearch}`}>
+                      <i className="material-icons">search</i>
+                      <input
+                        className={`${mainClass}__top__control`}
+                        name="search"
+                        type="text"
+                        placeholder="Search"
+                        onChange={this.handleChange}
+                        onClick={this.handleClick}
+                      />
+                      <button className="btn btn-sm">Go</button>
+                    </div>
+
+                    <div className={`${mainClass}__result ${classResult}`}>
+                      <ul>
+                        <li>
+                          <span>Users</span>
+                        </li>
+                        {this.renderLoopUsers()}
+                      </ul>
+                      <ul>
+                        <li>
+                          <span>Minsters</span>
+                        </li>
+                        {this.renderLoopMinisters()}
+                      </ul>
+                    </div>
                   </div>
                 </form>
               </div>
 
-              <div className="col-7 col-sm-6 col-lg-10 pr-0 text-right d-flex justify-content-end align-items-center">
+              <div className="col-7 col-sm-6 col-lg-3 pr-0 text-right d-flex justify-content-end align-items-center">
                 <div className="nav-user">
                   <div onClick={e => this.handleOpen("user")}>
                     <UserImage />
