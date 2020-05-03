d6e8eacadace716df1654a39fb131d8ca3720c11
Pankaj Jasoria <pnkj_jsr@yahoo.co.in>
2020-05-03 13:19:52 +0530

    feat-64: Homepage | mobile navigation added.

---
+++ components/Nav/Mobile/index.jsx
@@ -0,0 +1,99 @@
+import React, { Component, Fragment } from "react";
+import { connect } from "react-redux";
+import Link from "next/link";
+
+import "./style.scss";
+const logoImg = "/static/graphics/icon-57.png";
+
+class MobileNav extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <Fragment>
+        <nav className="nav-mobile">
+          <ul>
+            <li className="nav-logo">
+              <Link href="/mobile/welcome">
+                <a>
+                  <figure>
+                    <img src={logoImg} alt="Sochke Logo" />
+                  </figure>
+                  <figcaption>Sochke</figcaption>
+                </a>
+              </Link>
+            </li>
+            <li>
+              <Link href="/mobile/neta">
+                <a>
+                  <label htmlFor="Rate your neta">Rate your neta</label>
+                </a>
+              </Link>
+            </li>
+            <li>
+              <Link href="/mobile/contribute">
+                <a>
+                  <label htmlFor="Validate contribution">
+                    Validate contribution
+                  </label>
+                </a>
+              </Link>
+            </li>
+            <li>
+              <Link href="/covid">
+                <a>
+                  <label htmlFor="Covid-19">Covid-19</label>
+                </a>
+              </Link>
+            </li>
+
+            <li className="heading">
+              <label htmlFor="Covid-19">About Us</label>
+            </li>
+            <li>
+              <Link href="/about">
+                <a>
+                  <label htmlFor="Covid-19">About Us</label>
+                </a>
+              </Link>
+            </li>
+            <li>
+              <Link href="/terms">
+                <a>
+                  <label htmlFor="Covid-19">Terms</label>
+                </a>
+              </Link>
+            </li>
+            <li>
+              <Link href="/privacy-policy">
+                <a>
+                  <label htmlFor="Covid-19">Privacy policy</label>
+                </a>
+              </Link>
+            </li>
+            <li>
+              <Link href="/cookies">
+                <a>
+                  <label htmlFor="Covid-19">Cookies</label>
+                </a>
+              </Link>
+            </li>
+            <li>
+              <Link href="/feedback">
+                <a>
+                  <label htmlFor="Covid-19">Feedback</label>
+                </a>
+              </Link>
+            </li>
+          </ul>
+        </nav>
+      </Fragment>
+    );
+  }
+}
+
+export default connect((state) => state)(MobileNav);
--- pages/mobile/components/Header/index.jsx
+++ pages/mobile/components/Header/index.jsx
@@ -1,12 +1,62 @@
 import React, { Component } from "react";
 import Link from "next/link";
+
+import Button from "components/Form/Button";
+import Drawer from "components/Drawer";
+import MobileNav from "components/Nav/Mobile";
+
 import "./style.scss";
 
 export default class Header extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      openDrawer: "",
+    };
+  }
+
+  handleOpen = (e) => {
+    let name = `${e}Drawer`;
+    this.setState({
+      [name]: "open",
+    });
+  };
+
+  handleClose = (e) => {
+    let name = `${e}Drawer`;
+    this.setState({
+      [name]: "",
+    });
+  };
+
   render() {
     const mainClass = "component_header";
+    const { openDrawer } = this.state;
     return (
       <header className={mainClass}>
+        <div className={`${mainClass}__top`}>
+          <div className="navigation">
+            <i
+              className="material-icons"
+              onClick={(e) => this.handleOpen("open")}
+            >
+              menu
+            </i>
+            <Drawer
+              side="left"
+              open={openDrawer}
+              action={(e) => this.handleClose("open")}
+            >
+              <MobileNav />
+            </Drawer>
+          </div>
+
+          <div className="action d-none">
+            <Button text="Vote Neta" variant="btn-sm btn-outline-primary" />
+          </div>
+        </div>
+
         <div className="logo">
           <Link href="/">
             <a>{process.env.domain}</a>
