cacac32cf01e34b1d588472bbb5304bf78a26230
Pankaj Jasoria <pnkj_jsr@yahoo.co.in>
2020-05-24 22:31:47 +0530

    feat-68: neta mobile section api call done.

--- pages/minister/neta.jsx
+++ pages/minister/neta.jsx
@@ -1,10 +1,11 @@
 import React, { Component, Fragment } from "react";
+import { FaInfoCircle } from "react-icons/fa";
 import { connect } from "react-redux";
 import { bindActionCreators } from "redux";
 import ministerActions from "./action";
-import { service } from "apiConnect";
 
 import PageLoader from "components/Loader/page";
+import Button from "components/Form/Button";
 
 import "./style.scss";
 
@@ -19,8 +20,6 @@ class Neta extends Component {
 
     this.state = {
       query: props.queryName,
-      name: "",
-      testing: "",
     };
   }
 
@@ -39,6 +38,7 @@ class Neta extends Component {
         liabilities: props.minister.liabilities,
         address: props.minister.address,
         state: props.minister.state,
+        photoDisplay: props.minister.photoDisplay,
         photoUrl: props.minister.photoUrl,
         name: props.minister.name,
         assets: props.minister.assets,
@@ -58,9 +58,21 @@ class Neta extends Component {
     ministerAction.prefetchNetaData(query);
   }
 
+  handleDescShow = () => {
+    this.setState({
+      classDesc: "show",
+    });
+  };
+
+  handleDescHide = () => {
+    this.setState({
+      classDesc: "hide",
+    });
+  };
+
   render() {
     const mainClass = "neta";
-    const { name } = this.state;
+    const { name, photoDisplay, party, type, classDesc } = this.state;
 
     if (!name) {
       return <PageLoader />;
@@ -68,7 +80,59 @@ class Neta extends Component {
       return (
         <Fragment>
           <div className={mainClass}>
-            <div className="pj">{name}</div>
+            <div className={`${mainClass}__item`}>
+              <figure className="image">
+                <img src={photoDisplay} alt="" />
+              </figure>
+
+              <div className={`desc ${classDesc}`}>
+                <div className="hide" onClick={this.handleDescHide}>
+                  <span className="material-icons">cancel</span>
+                </div>
+                PJ
+              </div>
+
+              <div className="bot">
+                <FaInfoCircle className="info" onClick={this.handleDescShow} />
+
+                <div className="detail">
+                  <p>{name}</p>
+                </div>
+              </div>
+            </div>
+
+            <div className={`${mainClass}__action`}>
+              <Button
+                text="I Believe"
+                variant="btn-success"
+                // action={(e) => this.handleVote(contribute.id, "agree")}
+              />
+              <Button
+                text="I Won't"
+                variant="btn-danger"
+                // action={(e) => this.handleVote(contribute.id, "disagree")}
+              />
+              <Button
+                text="Pass"
+                variant="btn-outline-primary"
+                // action={(e) => this.handleVote(contribute.id, "pass")}
+              />
+            </div>
+
+            <div className={`${mainClass}__bot`}>
+              <div className="action">
+                <label>
+                  <small>I want say</small>
+                  <br /> Neta Se
+                </label>
+                <div className="add" onClick={this.handleRegister}>
+                  <img
+                    src="https://firebasestorage.googleapis.com/v0/b/sochke-web.appspot.com/o/cdn%2Fintro%2Fadd.gif?alt=media"
+                    alt="Add Contribute"
+                  />
+                </div>
+              </div>
+            </div>
           </div>
         </Fragment>
       );
